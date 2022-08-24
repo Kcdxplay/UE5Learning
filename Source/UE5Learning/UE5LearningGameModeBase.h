cf4fb4b32831b6c07ac0d47dedc230faaa712d81
Kcdxplay <kcdxplay@gmail.com>
2022-08-24 15:51:39 +0800

    Content Added
    Just all the new stuff from course
    kcdxplay-2022-0821-1551

---
+++ Source/UE5Learning/UE5LearningGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5LearningGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5LEARNING_API AUE5LearningGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
